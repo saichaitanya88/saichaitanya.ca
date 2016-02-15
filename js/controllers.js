6e78be3ebe9e4a923ead996f6f46eaff82f6dbbf
Sai Chaitanya <saichaitanya88@gmail.com>
2016-02-15 17:58:03 -0500

    ga

--- js/controllers.js
+++ js/controllers.js
@@ -5,9 +5,23 @@ ctrls.controller('MainCtrl', ['$scope', '$http', function($scope, $http) {
 	$scope.email = "Click to reveal";
 	$scope.revealPhone = function() {
 		$scope.phone = '416 897 1656';
+		var eventDate = {
+		  hitType: 'click',
+		  eventCategory: 'revealPhone',
+		  eventAction: 'click',
+		  eventLabel: window.location.hash
+		};
+  	ga('send', eventDate);
 	}
 	$scope.revealEmail = function() {
 		$scope.email = 'saichaitanya88@gmail.com';
+		var eventDate = {
+		  hitType: 'click',
+		  eventCategory: 'revealEmail',
+		  eventAction: 'click',
+		  eventLabel: window.location.hash
+		};
+  	ga('send', eventDate);
 	}
   // $(document).ready(function(){
   // });
