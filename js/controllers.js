466645cf7d119be21cbc000a1fde2df929b38340
Sai Chaitanya <saichaitanya88@gmail.com>
2016-02-16 22:30:45 -0500

    work-exp update

--- js/controllers.js
+++ js/controllers.js
@@ -5,35 +5,17 @@ ctrls.controller('MainCtrl', ['$scope', '$http', function($scope, $http) {
 	$scope.email = "Click to reveal";
 	$scope.revealPhone = function() {
 		$scope.phone = '416 897 1656';
-		var eventDate = {
-		  hitType: 'event',
-		  eventCategory: 'revealPhone',
-		  eventAction: 'click',
-		  eventLabel: window.location.hash
-		};
-  	ga('send', eventDate);
+  	ga('send', 'event', 'revealPhone', 'click', window.location.hash);
 	}
 	$scope.revealEmail = function() {
 		$scope.email = 'saichaitanya88@gmail.com';
-		var eventDate = {
-		  hitType: 'event',
-		  eventCategory: 'revealEmail',
-		  eventAction: 'click',
-		  eventLabel: window.location.hash
-		};
-  	ga('send', eventDate);
+  	ga('send', 'event', 'revealEmail', 'click', window.location.hash);
 	}
   // $(document).ready(function(){
   // });
   angular.element(document).ready(function(){
   	$('.collapsible').collapsible();
-  	var eventDate = {
-		  hitType: 'event',
-		  eventCategory: window.location.hash,
-		  eventAction: 'click',
-		  eventLabel: window.location.hash
-		};
 		//console.log(eventDate);
-  	ga('send', eventDate);
+  	ga('send', 'event', window.location.hash, 'click', window.location.hash);
   });
 }]);
